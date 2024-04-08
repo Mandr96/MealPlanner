185ed94a97c2ed0cb62a2643660d1484ed426390
Mandr96 <98476394+Mandr96@users.noreply.github.com>
2024-04-08 17:33:03 +0200

    Spring init

---
+++ MealPlannerServer/src/test/java/com/mealplanner/demo/MealPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mealplanner.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
